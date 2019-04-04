016ee6b24f061dc5385582b9617c55d9e0d011f6
Tiffany Zhong <tiffzhong@gmail.com>
2019-04-03 23:25:01 -0700

    author list running. working on titles

--- src/Components/Authors/AuthorsList.js
+++ src/Components/Authors/AuthorsList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import AuthorNames from "./AuthorNames";
 var parseString = require("xml2js").parseString;
 
 class AuthorsList extends Component {
@@ -15,14 +16,10 @@ class AuthorsList extends Component {
   componentDidMount() {
     this.getAuthors();
   }
-  // componentDidUpdate(prevProps) {
-  //   if (prevProps.match.path !== this.props.match.path) {
-  //     this.filtered();
-  //   }
-  // }
 
   getAuthors = () => {
     let auth = [];
+    let another = [];
     axios
       .get(
         "http://export.arxiv.org/api/query?search_query=all:psychiatry+OR+all:therapy+OR+all:data+science+OR+all:machine+learning&sortBy=lastUpdatedDate&sortOrder=descending&max_results=40"
@@ -30,50 +27,88 @@ class AuthorsList extends Component {
       .then(data => {
         parseString(data.data, function(err, result) {
           auth.push(result.feed.entry);
+          console.log(result);
         });
         let mapper = auth[0].map(g => g.author);
-        let names = mapper.map(v => v.map(t => t.name));
-        console.log(names, "nems");
-        this.setState({
-          authors: names
-        });
+        console.log(mapper, "mapper");
+        let names = mapper.map(v =>
+          v.map(t => {
+            // console.log(t, "tea");
+            axios
+              .get(
+                `http://export.arxiv.org/api/query?search_query=au:"${
+                  t.name
+                }"&sortBy=lastUpdatedDate&sortOrder=descending&max_results=40`
+              )
+              .then(data => {
+                parseString(data.data, (err, result) => {
+                  let authorsCopy = this.state.authors.concat();
+                  authorsCopy.push({
+                    results: result.feed.entry,
+                    name: t.name
+                  });
+                  this.setState({
+                    authors: authorsCopy
+                  });
+                });
+              });
+          })
+        );
+        // return;
+        // setTimeout(function() {
+        // console.log(another, "1");
+        // }, 5000);
+
+        // let mappedAnother = another.map(o => {
+        //   console.log(o, "oh");
+        //   return { results: o.results.length, name: o.name };
+        // });
+        // return mappedAnother;
+
+        // console.log(names, "woieur");
+        // this.setState({
+        //   authors: names
+        // });
       })
       .catch(err => console.log("author get", err));
   };
 
   searchAuthor = authorName => {
     let searched = [];
+    console.log(authorName, "namehhh");
     axios
-      .get(
-        `http://export.arxiv.org/api/query?search_query=au:${authorName.replace(
-          /\s/g,
-          "+"
-        )}`
-      )
+      .get(`http://export.arxiv.org/api/query?search_query=au:"${authorName}"`)
       .then(data => {
         parseString(data.data, function(err, result) {
           searched.push(result.feed.entry);
         });
         console.log(searched);
-        this.setState({
-          clickedAuthor: searched
-        });
+
+        this.setState(
+          {
+            clickedAuthor: searched
+          },
+          this.props.history.push(`/authorsName/${authorName}`)
+        );
       })
       .catch(err => console.log("search get", err));
   };
 
   render() {
-    console.log(this.state.clickedAuthor);
-    return (
-      <div>
-        <Link
-          to={`/authorsName/${this.state.clickedAuthor}`}
-          onClick={() => this.searchAuthor(this.state.clickedAuthor)}
-        >
-          {this.state.authors}
-        </Link>
-      </div>
-    );
+    // <AuthorNames searchedData={this.state.clickedAuthor} />;
+    // let clickedAuthorLink = this.state.clickedAuthor.map(
+    //   info => console.log(info, "info")
+    // <div>
+    //   <AuthorNames title={info[0].title} />
+    // </div>
+
+    let mappedAuthors = this.state.authors.map(author => (
+      <button onClick={() => this.searchAuthor(author.name)}>
+        {author.name} {author.results !== undefined ? author.results.length : 0}
+      </button>
+    ));
+
+    return <div>{mappedAuthors}</div>;
   }
 }
 export default AuthorsList;
